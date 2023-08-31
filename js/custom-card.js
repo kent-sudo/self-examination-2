12b6d5223c36c77933083084e516f8cce5141da0
kent-sudo <72906102+kent-sudo@users.noreply.github.com>
2023-08-31 21:02:12 +0800

    v1

---
+++ js/custom-card.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+    // Event handler for clicking a card
+
+});
---
+++ js/summernote-init.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    $('#summernote_kent').summernote({
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: true                  // set focus to editable area after initializing summernote
+    });
+});
